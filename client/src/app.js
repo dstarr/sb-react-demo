ee9ff2824241bd56a56b7afe27f3071fb62043ef
David Starr <dxstarr@godaddy.com>
2016-08-16 17:47:20 -0700

    Got the Header Going as I hoped. Using Grid layouts

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/less/bootstrap.less'
+import 'bootstrap/less/bootstrap.less';
 
+import MainContainer from './components/mainContainer';
+
+// import Hello from './components/hello';
 
 const node1 = document.getElementById('app');
 
 if (node1) {
-  ReactDOM.render( <h2>Hello World!</h2>, node1);
+  ReactDOM.render( <MainContainer />, node1);
 }
---
+++ client/src/components/mainContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './header';
+
+export default class MainContainer extends React.Component {
+
+  render = () => {
+    return (
+      <div>
+
+        <Header />
+
+      </div>
+    );
+  }
+}
