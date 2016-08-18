7adadf3ab1d11de004c4496281cc5c8b887623f1
David Starr <dxstarr@godaddy.com>
2016-08-18 02:56:54 -0700

    all is well and done. FInal state for demo

---
+++ client/src/helloComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class HelloComponent extends React.Component {
+
+  render = () => {
+    
+    return (
+      <div style={{color:'#ffffff'}}>
+
+        <h2>Hello, from React</h2>
+
+        <p>
+          Now let's make something cool!
+        </p>
+
+      </div>
+    );
+    
+  }
+}
+
